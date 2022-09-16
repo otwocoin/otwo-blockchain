09674eed2ecd9c1065f8867e863c016d5b8ea681
Awet Tsegazeab <avvvet@gmail.com>
2022-09-16 21:00:13 +0300

    network model

--- go.mod
+++ go.mod
@@ -9,8 +9,11 @@ require (
 )
 
 require (
+	github.com/avvvet/merkletree v0.0.1 // indirect
+	github.com/avvvet/oxygen v0.0.0-20220913185515-e5b08e02f082 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/containerd/cgroups v1.0.4 // indirect
@@ -26,6 +29,7 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -52,6 +56,7 @@ require (
 	github.com/libp2p/go-libp2p-asn-util v0.2.0 // indirect
 	github.com/libp2p/go-libp2p-core v0.20.0 // indirect
 	github.com/libp2p/go-libp2p-kbucket v0.4.7 // indirect
+	github.com/libp2p/go-libp2p-pubsub v0.8.0 // indirect
 	github.com/libp2p/go-libp2p-record v0.2.0 // indirect
 	github.com/libp2p/go-msgio v0.2.0 // indirect
 	github.com/libp2p/go-nat v0.1.0 // indirect
@@ -96,12 +101,14 @@ require (
 	github.com/raulk/go-watchdog v1.3.0 // indirect
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/whyrusleeping/go-keyspace v0.0.0-20160322163242-5b898ac5add1 // indirect
+	github.com/whyrusleeping/timecache v0.0.0-20160911033111-cfcb2f1abfee // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.22.0 // indirect
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
+	"github.com/avvvet/oxygen/pkg/blockchain"
+	"github.com/avvvet/oxygen/pkg/wallet"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/multiformats/go-multiaddr"
+	net "github.com/otwocoin/otwo-blockchain/internal/pkg/net"
+	util "github.com/otwocoin/otwo-blockchain/internal/pkg/util"
+	"go.uber.org/zap"
+)
+
+var (
+	logger, _ = zap.NewProduction() // or NewProduction, or NewDevelopment
 )
 
 type addrList []multiaddr.Multiaddr
@@ -28,16 +41,16 @@ type Config struct {
 func main() {
 	config := Config{}
 
-	flag.StringVar(&config.Rendezvous, "meet", "oxygen", "peer joining place")
+	flag.StringVar(&config.Rendezvous, "meet", "otwo", "peer joining place")
 	flag.Int64Var(&config.Seed, "seed", 0, "0 is for random PeerID")
 	flag.Var(&config.DiscoveryPeers, "peer", "Perr address for peer discovery")
-	flag.StringVar(&config.ProtocolID, "protocolid", "/p2p/oxygen", "")
+	flag.StringVar(&config.ProtocolID, "protocolid", "/p2p/otwo", "")
 	flag.IntVar(&config.Port, "port", 0, "port for peer")
 	flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	h, err := NewHost(ctx, config.Seed, config.Port)
+	h, err := net.NewHost(ctx, config.Seed, config.Port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,14 +59,183 @@ func main() {
 		log.Printf("  %s/p2p/%s", addr, h.ID().Pretty())
 	}
 
-	dht, err := NewDHT(ctx, h, config.DiscoveryPeers)
+	/*create gossipSub */
+	pubSubInst, err := net.InitPubSub(ctx, h, "otwo")
+	if err != err {
+		logger.Sugar().Fatal("Error: creating pubsub", err)
+	}
+
+	dht, err := net.NewDHT(ctx, h, config.DiscoveryPeers)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go Discover(ctx, h, dht, config.Rendezvous)
+	go net.Discover(ctx, h, dht, config.Rendezvous)
+	go ListenBroadcast(ctx, pubSubInst.Subscription, h)
 	go ListPeers(h)
+
+	Process(ctx, pubSubInst)
 	run(h, cancel)
+}
+
+func Process(ctx context.Context, ps *net.PubsubInst) {
+	// Example: this will give us a 32 byte output
+	randomString, err := util.GenerateRandomString(32)
+	if err != nil {
+		// Serve an appropriately vague error to the
+		// user, but log the details internally.
+		panic(err)
+	}
+
+	//temp create wallet address and sign the first genesis transaction output
+	adamWallet := wallet.NewWallet()
+	eveWallet := wallet.NewWallet()
+
+	senderPK := util.Encode(adamWallet.PublicKey)
+	receiverPK := util.Encode(eveWallet.PublicKey)
+
+	natureRawTx := &wallet.RawTx{
+		SenderPublicKey:       senderPK,
+		SenderOxygenAddress:   adamWallet.OxygenAddress,
+		SenderRandomHash:      sha256.Sum256([]byte(randomString)),
+		ReceiverPublicKey:     senderPK,
+		ReceiverOxygenAddress: adamWallet.OxygenAddress,
+		Token:                 900,
+	}
+
+	txout := &blockchain.TxOutput{
+		RawTx:     natureRawTx,
+		Signature: natureRawTx.Sign(adamWallet.PrivateKey),
+	}
+
+	chain, err := blockchain.InitChain(txout)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer chain.Ledger.Db.Close()
+
+	rawTx1 := &wallet.RawTx{
+		SenderPublicKey:       senderPK,
+		SenderOxygenAddress:   adamWallet.OxygenAddress,
+		SenderRandomHash:      sha256.Sum256([]byte(randomString)),
+		Token:                 400,
+		ReceiverPublicKey:     receiverPK,
+		ReceiverOxygenAddress: eveWallet.OxygenAddress,
+	}
+
+	txout1 := &blockchain.TxOutput{
+		RawTx:     rawTx1,
+		Signature: rawTx1.Sign(adamWallet.PrivateKey),
+	}
+	tx1 := chain.NewTransaction(txout1)
+	chain.ChainBlock(`data {} `+strconv.Itoa(1), []*blockchain.Transaction{tx1})
+
+	rawTx2 := &wallet.RawTx{
+		SenderPublicKey:       senderPK,
+		SenderOxygenAddress:   adamWallet.OxygenAddress,
+		SenderRandomHash:      sha256.Sum256([]byte(randomString)),
+		Token:                 490,
+		ReceiverPublicKey:     receiverPK,
+		ReceiverOxygenAddress: eveWallet.OxygenAddress,
+	}
+
+	txout2 := &blockchain.TxOutput{
+		RawTx:     rawTx2,
+		Signature: rawTx2.Sign(adamWallet.PrivateKey),
+	}
+	tx2 := chain.NewTransaction(txout2)
+
+	//broadcast
+	newblock, err := chain.ChainBlock(`data {} `+strconv.Itoa(1), []*blockchain.Transaction{tx2})
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := json.Marshal(&net.BroadcastData{Type: "NEWBLOCK", Data: newblock})
+	if err != nil {
+		panic(err)
+	}
+	go Broadcaster(ctx, ps.Topic, data)
+
+	var i = 0
+	for {
+		data, err := chain.Ledger.Get([]byte(strconv.Itoa(i)))
+		if err != nil {
+			break
+		} else {
+			block := &blockchain.Block{}
+			err = json.Unmarshal(data, block)
+			if err != nil {
+				logger.Sugar().Fatal("unable to get block from store")
+			}
+
+			fmt.Printf("############## BlockHeight %v ############# \n", i)
+			fmt.Printf("Timestamp : %s \n", time.Unix(block.Timestamp, 0).Format(time.RFC3339))
+			fmt.Printf("Block Hash: %x\n", block.Hash)
+			fmt.Printf("Data: %s\n", block.Data)
+			fmt.Printf("Transaction ID: %x Inputs %+v  Outputs %+v\n", block.Transaction[0].ID, block.Transaction[0].Inputs, block.Transaction[0].Outputs)
+			fmt.Printf("Merkle root: %x\n", block.MerkleRoot)
+			fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+			fmt.Printf("Difficulty: %v\n", block.Difficulty)
+			fmt.Printf("Nonce: %v\n", block.Nonce)
+			fmt.Printf("BlockHeight: %v\n", block.BlockHeight)
+		}
+		i++
+	}
+
+}
+
+type Stream struct {
+	Type  string
+	Block *blockchain.Block
+}
+
+func ListenBroadcast(ctx context.Context, s *pubsub.Subscription, h host.Host) {
+	for {
+		msg, err := s.Next(ctx)
+		if err != nil {
+			fmt.Println("errrrrrrrr", err)
+		}
+
+		//only consider messages delivered by other peers
+		if msg.ReceivedFrom == h.ID() {
+			continue
+		}
+
+		var b *net.BroadcastData
+		json.Unmarshal([]byte(msg.Data), &b)
+
+		switch t := b.Type; t {
+		case "NEWBLOCK":
+			ValidateBlock(b.Data)
+			fmt.Println("Newwwwwwwwwwwwww arrived ******************************* ", msg.ReceivedFrom)
+		}
+
+	}
+}
+
+func ValidateBlock(b []byte) {
+	block := &blockchain.Block{}
+	err := json.Unmarshal(b, block)
+	if err != nil {
+		panic(err)
+	}
+    
+	block.
+}
+
+func Broadcaster(ctx context.Context, t *pubsub.Topic, data []byte) {
+	ticker := time.NewTicker(time.Second * 15)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			t.Publish(ctx, data)
+		}
+
+	}
+
 }
 
 func ListPeers(h host.Host) {
